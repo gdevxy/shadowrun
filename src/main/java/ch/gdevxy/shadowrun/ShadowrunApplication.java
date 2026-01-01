e12e91fa291d81288467f154a4141234262993d8
Gabriel Dinant <gdevxy@gmail.com>
2026-01-01 13:17:55 +0100

    first commit

---
+++ src/main/java/ch/gdevxy/shadowrun/ShadowrunApplication.java
@@ -0,0 +1,13 @@
+package ch.gdevxy.shadowrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShadowrunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/gdevxy/shadowrun/ShadowrunApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.gdevxy.shadowrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShadowrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
